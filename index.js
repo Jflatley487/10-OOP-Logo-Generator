33d37b9041b45fc324a61f40d754e9fa98321c91
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-04-15 16:18:22 -0500

    finished code

--- index.js
+++ index.js
@@ -1,6 +1,142 @@
 
+const { JSDOM } = require('jsdom');
 const inquirer = require('inquirer');
+const { create } = require('svg-captcha');
 const fs = require('fs');
+
+const { window } = new JSDOM();
+const document = window.document;
+
+
+/*const logoDiv = document.createElement('div');
+
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Logo</title>
+  <style>
+    svg {
+      display: block;
+      max-width: 100%;
+      height: auto;
+    }
+  </style>
+</head>
+<body>
+  <div id="logo"></div>
+  <script src="index.js"></script>
+</body>
+</html>
+`;
+
+logoDiv.innerHTML = html;
+
+const logoContainer = logoDiv.querySelector('#logo');*/
+
+function createLogo(answers) {
+    let svgLogo = '';
+
+    // Create logo shape
+    switch (answers.shape) {
+        case 'circle':
+            svgLogo += `<circle cx="50" cy="50" r="40" fill="${answers.shapeColor}"/>`;
+            break;
+        case 'triangle':
+            svgLogo += `<polygon points="50,10 10,90 90,90" fill="${answers.shapeColor}"/>`;
+            break;
+        case 'square':
+            svgLogo += `<rect x="10" y="10" width="80" height="80" fill="${answers.shapeColor}"/>`;
+            break;
+        default:
+            break;
+    }
+
+    // Create logo text
+    svgLogo += `<text x="50%" y="50%" fill="${answers.textColor}" font-size="50" text-anchor="middle" dominant-baseline="middle">${answers.title}</text>`;
+
+    // Add finishing touches
+    svgLogo = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">${svgLogo}</svg>`;
+
+    return svgLogo;
+}
+
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What text would you like for your logo? (up to 3 characters)',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter the color for the text (keyword or hexadecimal number):',
+    },
+    {
+        type: 'input',
+        name: 'shape',
+        message: 'Select a shape for your logo:',
+        choices: ['circle', 'triangle', 'square']
+
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the color for the shape (keyword or hexadecimal number):'
+    }
+]).then(answers => {
+    const captcha = create({
+        size: { width: 300, height: 200 },
+        ignoreChars: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+        noise: 3,
+        color: true,
+        background: '#fff',
+        fontSize: 80,
+        charPreset: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    });
+
+    captcha.text = answers.text;
+    captcha.textStyle = `fill:${answers.textColor}`;
+    captcha.data = captcha.data.replace(/#000/g, answers.shapeColor);
+
+    fs.writeFile('logo.svg', captcha.data, function (err) {
+        if (err) throw err;
+        console.log('Generated logo.svg');
+
+        const svgLogo = createLogo(answers);
+
+        
+        const html = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Logo</title>
+            <style>
+                svg {
+                    display: block;
+                    max-width: 100%;
+                    height: auto;
+                }
+            </style>
+        </head>
+        <body>
+            <div>
+                <img src="data:image/svg+xml;base64,${Buffer.from(svgLogo).toString('base64')}" />
+            </div>
+        </body>
+        </html>
+        `;
+
+        fs.writeFile('index.html', html, function (err) {
+            if (err) throw err;
+            console.log('Generated index.html');
+
+
+    });
+});
+});
+
 
 /*GIVEN a command-line application that accepts user input
 WHEN I am prompted for text
@@ -16,53 +152,3 @@ THEN an SVG file is created named `logo.svg`
 AND the output text "Generated logo.svg" is printed in the command line
 WHEN I open the `logo.svg` file in a browser
 THEN I am shown a 300x200 pixel image that matches the criteria I entered */
-
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What text would you like for your logo? (up to 3 characters)',
-    },
-    {
-        type: 'input',
-        name: 'text color',
-        message: 'Please enter text color keyword or hexadecimal number:',
-    },
-    {
-        type: 'input',
-        name: 'shape',
-        message: 'What shape would you like the logo to be?',
-    
-    },
-    {
-        type: 'input',
-        name: 'shape color',
-        message: 'What color would you like the shape to be?'
-    }
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log('README.md file generated!')
-    );
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        const readme = `
-        # ${answers.title}
-        ## Description
-        ${answers.description}
-        ## Installation
-        ${answers.installation}
-        ## Screenshot
-        ${answers.screenshot}
-        `;
-            writeToFile('README.md', readme);
-    });
-}
-
-// Function call to initialize app
-init();
